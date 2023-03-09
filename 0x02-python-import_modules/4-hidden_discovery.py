0f6103ee0e97e7c5de6baac159dc3d885658a239
dash201 <liamidisad24@gmail.com>
2023-03-09 05:14:42 -0800

    4. Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from hidden_4 import *
+    import re
+
+    def Filter(data):
+        return [val for val in data
+                if not re.search(r'^_', val)]
     liste = dir()
-    print(liste[-3])
-    print(liste[-2])
-    print(liste[-1])
+    for c in Filter(liste):
+        print("{}".format(c))
