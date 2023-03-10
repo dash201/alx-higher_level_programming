ffc3ed62d528c96534d307fb0454037f74077c96
dash201 <liamidisad24@gmail.com>
2023-03-10 04:05:41 -0800

    5. Can you C me now?

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    def no_c(my_string):
+        new_str = ""
+        size = len(my_string)
+        for i in range(0, size):
+            if my_string[i] in ["c", "C"]:
+                new_str += ""
+            else:
+                new_str += my_string[i]
+        return new_str
