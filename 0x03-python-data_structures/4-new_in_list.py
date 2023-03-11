314efa20a01d4509d9e57f6ba159e63d3f4980e8
dash201 <liamidisad24@gmail.com>
2023-03-11 11:01:53 -0800

    4. Replace in a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-
-    def new_in_list(my_list, idx, element):
-        size = len(my_list)
-        new_list = []
-        if idx < 0:
-            return (None)
-        elif idx > size:
-            return (None)
-        else:
-            for i in range(0, size):
-                if i == idx:
-                    new_list.append(element)
-                else:
-                    new_list.append(my_list[i])
-            return (new_list)
+def new_in_list(my_list, idx, element):
+    size = len(my_list)
+    new_list = []
+    if idx < 0:
+        return (None)
+    elif idx > size:
+        return (None)
+    else:
+        for i in range(0, size):
+            if i == idx:
+                new_list.append(element)
+            else:
+                new_list.append(my_list[i])
+                return (new_list)
