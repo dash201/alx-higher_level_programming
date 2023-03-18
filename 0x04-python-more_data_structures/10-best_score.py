4a2fe6642042537ec481b7fbf3aff3e1ebd2717c
dash201 <liamidisad24@gmail.com>
2023-03-17 19:56:01 -0700

    10. Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,6 +3,6 @@ def best_score(a_dictionary):
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
     new = []
-    for c in a_dictionary:
-        new.append(a_dictionary[c])
-    return max(new)
+    for c in sorted(a_dictionary.item, reversed=True):
+        new.append(c)
+    return c[0]
